852ac51405042941fc5b0542b408138808909016
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-13 23:22:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
-# Code your solution here!
+def run_guessing_game
+  puts "Pick a number between 1 and 6"
+  
+  ran_num = rand(6) + 1
+  
+  input = gets.chomp
+  
+  if input == ran_num.to_s
+    puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry!  The computer guessed #{ran_num}."
+  end
+end
